8ee77ab14522052d7ca15a54bd718dbd068a1fb0
enuchsa <enuchsa@gmail.com>
2023-12-27 12:57:49 -0300

    mexend...

--- heroes/models.py
+++ heroes/models.py
@@ -1,22 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 
 
 class Hero(models.Model):
-    name = models.CharField(max_lentgh=150, blank=False, null=False)
-    description = models.CharField(max_lentgh=400, blank=False, null=False)
-    image = models.CharField(max_lentgh=150, blank=False, null=False)
+    name = models.CharField(max_length=150, blank=False, null=False)
+    description = models.CharField(max_length=400, blank=False, null=False)
+    image = models.CharField(max_length=150, blank=False, null=False)
     
     def __str__(self):
         return self.name
     
     class Meta:
         ordering = ['name']
+        db_table = 'hero'
     
 class Group(models.Model):
-    name = models.CharField(max_lentgh=150, blank=False, null=False)
-    description = models.CharField(max_lentgh=300, blank=False, null=False)
-    heroes = models.ForeignKey(Hero, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, blank=False, null=False)
+    description = models.CharField(max_length=300, blank=False, null=False)
+    hero = models.JSONField()
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
+    
+    class Meta:
+        db_table = 'group'
+        # unique_together = ('user', 'hero', 'id')
+
+    @receiver(pre_save, sender=Group)
+    def validate_unique_hero_ids(sender, instance, **kwargs):
+        # Obtém o ID do grupo atual, excluindo-o da comparação
+        grupo_atual_id = instance.id if instance.id is not None else None
+
+        # Verifica se os IDs de heróis estão em algum outro grupo
+        outros_grupos = Group.objects.exclude(id=grupo_atual_id).filter(
+            hero__overlap=instance.hero
+        )
+
+        if outros_grupos.exists():
+            ids_em_outro_grupo = outros_grupos.values_list('hero', flat=True).get()
+            ids_em_conflito = set(instance.hero).intersection(ids_em_outro_grupo)
+            raise ValueError(f"IDs de heróis {ids_em_conflito} já estão em outro grupo.")
+        
+
+
--- heroes/views.py
+++ heroes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from heroes.models import Hero, Group
+from heroes.serializers import HeroSerializer, GroupSerializer
 
-# Create your views here.
+
+class HeroViewSet(viewsets.ModelViewSet):
+    queryset = Hero.objects.all()
+    serializer_class = HeroSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+    
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    # permission_classes = [permissions.IsAuthenticated]
