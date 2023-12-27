63e0f78b6513e6188baf65329bf4cdf5553d9507
enuchsa <enuchsa@gmail.com>
2023-12-27 12:01:43 -0300

    Models: Hero e Group fase - 1

--- heroes/models.py
+++ heroes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Hero(models.Model):
+    name = models.CharField(max_lentgh=150, blank=False, null=False)
+    description = models.CharField(max_lentgh=400, blank=False, null=False)
+    image = models.CharField(max_lentgh=150, blank=False, null=False)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+    
+class Group(models.Model):
+    name = models.CharField(max_lentgh=150, blank=False, null=False)
+    description = models.CharField(max_lentgh=300, blank=False, null=False)
+    heroes = models.ForeignKey(Hero, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
